aa9cef6baed3f9697dc30f5d841373d0e59a2906
Himanshu <goyalhimanshu424@gmail.com>
2022-01-13 00:33:33 +0530

    second commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+linebreak-Style ["error", "unix", "windows"];
+module.exports = {
+    env : {
+        browser : true,
+        node : true,
+        es2020 : true,
+    },
+    extends : ['airbnb-base' , 'prettier'],
+    parserOptions : {
+        sourceType : 'module',
+        ecmaVersion : 11,
+    },
+    rules :{
+        'no-console' : 0,
+    }
+};
